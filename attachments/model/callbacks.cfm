liquifusion/cfwheels-attachments

commit 85ca862a866fcfdedc16d64f04b80121fa2865b4

Zero-length file uploads throw error.

attachments/model/callbacks.cfm
@@ -176,8 +176,26 @@
 			, result = "returnValue"
 			, nameconflict = "overwrite"
 		};
+
+		try
+		{
+			return $file(argumentCollection=fileArgs);
+		}
+		catch (any e)
+		{
+			// This is only tested on CF9. If you get an error on Railo, see if there is an equivalent error to
+			//   catch in another `catch` block.
+			if (e.Detail contains "zero-length")
+			{
+				this.addError(property=arguments.property, message="Can't upload an empty file");
+				return false;
+			}
+			else
+			{
+				$throw(argumentCollection=e);
+			}
+		}
 	</cfscript>
-	<cfreturn $file(argumentCollection=fileArgs) />
 </cffunction>
 
 <cffunction name="$saveFileToStorage" access="public" output="false" returntype="struct">

commit dd016b7a02d00055e8ff09e55d7124e930837a7b

Fix bug introduced in 85ca862a866fcfdedc16d64f04b80121fa2865b4 when file is zero-length
Exception: The value returned from the $saveFileToTempDirectory function is not of type struct.

attachments/model/callbacks.cfm
@@ -87,6 +87,11 @@
 		if (StructKeyExists(this, loc.attachment.property & "$attachment"))
 		{
 			loc.file = $saveFileToTempDirectory(argumentCollection=arguments);
+
+			if (IsBoolean(loc.file)) {
+				return loc.file;
+			}
+
 			loc.filePath = Replace(GetTempDirectory() & loc.file.ServerFile, "\", "/", "all");
 			
 			// check to make sure we don't have a bad file
@@ -165,7 +170,7 @@
 	</cfscript>
 </cffunction>
 
-<cffunction name="$saveFileToTempDirectory" access="public" output="false" returntype="struct">
+<cffunction name="$saveFileToTempDirectory" access="public" output="false">
 	<cfargument name="property" type="string" required="true" />
 	<cfscript>
 		// Set the file in a temp location to verify it's not evil